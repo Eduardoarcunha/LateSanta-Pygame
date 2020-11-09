6723849b78e30f0d119c5197ef0fec4481aa7853
unknown <eduardoarc@al.insper.edu.br>
2020-11-09 18:16:32 -0300

    Etapas bases do jogo

---
+++ Jogo.py
@@ -0,0 +1,37 @@
+#Projeto Final Dessoft
+#Alunos: Eduardo Araujo, Luiza Manzig, Renato Falcão
+
+#Import Bibliotecas
+import pygame
+import sys
+import os
+
+pygame.init()
+
+WIDTH = 1200
+HEIGHT = 800
+
+janela = pygame.display.set_mode([WIDTH,HEIGHT])
+
+BACKGROUND = os.path.join('Assets','Images','BG_02.png')
+
+
+try:
+    background = pygame.image.load(BACKGROUND).convert()
+except pygame.error:
+    print('Problema no load da imagem')
+    sys.exit()
+
+janela.blit(background,[0,0])
+
+pygame.display.flip()
+
+#Game Loop
+game_on = True
+
+while game_on:
+    eventos = pygame.event.get()
+    for evento in eventos:
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            game_on = False
