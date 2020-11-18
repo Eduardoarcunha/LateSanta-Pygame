ea6457d4591c4e8d6e31e7b9fdc082d3d427fa53
Luisa Kyrillos <luisamk@al.insper.edu.br>
2020-11-18 10:54:30 -0300

    limitando movimento do santa

--- classes.py
+++ classes.py
@@ -116,6 +116,11 @@ class Santa(pygame.sprite.Sprite):
         self.rect.x += self.speedx
         self.rect.y += self.speedy
 
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+        
 
         if self.rect.centery < 400:
             self.speedy += GRAVITY
